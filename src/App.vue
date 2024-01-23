fc110d0860dde2e614f7c23236406349a31e245a
Kyle Anthony Nierras <kyleanthony47@gmail.com>
2024-01-23 10:39:45 +0800

    Fix on Routerr

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { useRoute, RouterLink, RouterView } from 'vue-router';
+import { computed } from 'vue';
+import Sidebar from '@/components/Sidebar.vue';
+
+const route = useRoute();
+const showSidebar = computed(() => route.name !== 'Login');
 </script>
 
 <template>
-
-  <div class="router">
-    <RouterView />
+  <div>
+    <Sidebar v-if="showSidebar" />
+    <div :class="{ 'router': showSidebar }">
+      <RouterView />
+    </div>
   </div>
 </template>
 
 <style scoped>
-
+.router {
+  margin-top: 10px;
+  margin-left: 300px;
+}
 </style>
